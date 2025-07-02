960b08496c93e59717f8795ee1257405027fb673
Aaron <4709780+2manyvcos@users.noreply.github.com>
2025-07-02 11:48:34 +0000

    no need to panic

--- main.go
+++ main.go
@@ -22,32 +22,38 @@ func main() {
 
 	apiUrl := os.Getenv("API_URL")
 	if apiUrl == "" {
-		panic("missing API url")
+		fmt.Println("Missing API url")
+		os.Exit(1)
 	}
 
 	apiUser := os.Getenv("API_USER")
 	if apiUser == "" {
-		panic("missing API user")
+		fmt.Println("Missing API user")
+		os.Exit(1)
 	}
 
 	apiPassword := os.Getenv("API_PASSWORD")
 	if apiPassword == "" {
-		panic("missing API password")
+		fmt.Println("Missing API password")
+		os.Exit(1)
 	}
 
 	packageOwner := os.Getenv("PACKAGE_OWNER")
 	if packageOwner == "" {
-		panic("missing package owner")
+		fmt.Println("Missing package owner")
+		os.Exit(1)
 	}
 
 	packageName := os.Getenv("PACKAGE_NAME")
 	if packageName == "" {
-		panic("missing package name")
+		fmt.Println("Missing package name")
+		os.Exit(1)
 	}
 
 	packageVersion := os.Getenv("PACKAGE_VERSION")
 	if packageVersion == "" {
-		panic("missing package version")
+		fmt.Println("Missing package version")
+		os.Exit(1)
 	}
 
 	fail := os.Getenv("FAIL")
@@ -59,10 +65,12 @@ func main() {
 		defer func() {
 			response, err := http.Post(fmt.Sprintf("%s/api/v1/var?key=%s", reeveAPI, url.QueryEscape(resultVar)), "text/plain", strings.NewReader(result))
 			if err != nil {
-				panic(fmt.Sprintf("error setting result var - %s", err))
+				fmt.Printf("Error setting result var - %s\n", err)
+				os.Exit(1)
 			}
 			if response.StatusCode != http.StatusOK {
-				panic(fmt.Sprintf("setting result var returned status %v", response.StatusCode))
+				fmt.Printf("Setting result var returned status %v\n", response.StatusCode)
+				os.Exit(1)
 			}
 			fmt.Printf("Set %s=%s\n", resultVar, result)
 		}()
@@ -72,17 +80,20 @@ func main() {
 	requestUrl := fmt.Sprintf("%s/api/v1/packages/%s/generic/%s/%s/files", apiUrl, url.PathEscape(packageOwner), url.PathEscape(packageName), url.PathEscape(packageVersion))
 	request, err := http.NewRequest(http.MethodGet, requestUrl, nil)
 	if err != nil {
-		panic(fmt.Sprintf(`error fetching package file list - %s`, err))
+		fmt.Printf("Error fetching package file list - %s\n", err)
+		os.Exit(1)
 	}
 	request.SetBasicAuth(apiUser, apiPassword)
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		panic(fmt.Sprintf(`error fetching package file list - %s`, err))
+		fmt.Printf("Error fetching package file list - %s\n", err)
+		os.Exit(1)
 	}
 	if response.StatusCode != http.StatusOK {
 		response.Body.Close()
 		if response.StatusCode != http.StatusNotFound {
-			panic(fmt.Sprintf("fetching package file list returned status %v", response.StatusCode))
+			fmt.Printf("Fetching package file list returned status %v\n", response.StatusCode)
+			os.Exit(1)
 		}
 	} else {
 		var packageList []struct {
@@ -91,7 +102,8 @@ func main() {
 		err := json.NewDecoder(response.Body).Decode(&packageList)
 		response.Body.Close()
 		if err != nil {
-			panic(fmt.Sprintf(`error fetching package file list - %s`, err))
+			fmt.Printf("Error fetching package file list - %s\n", err)
+			os.Exit(1)
 		}
 		packageFiles = make([]string, 0, len(packageList))
 		for _, packageFile := range packageList {
@@ -101,7 +113,8 @@ func main() {
 
 	filePatterns, err := shlex.Split(os.Getenv("FILES"))
 	if err != nil {
-		panic(fmt.Sprintf("error parsing file pattern list - %s", err))
+		fmt.Printf("Error parsing file pattern list - %s\n", err)
+		os.Exit(1)
 	}
 	var found bool
 L:
@@ -109,7 +122,8 @@ L:
 		for _, filename := range packageFiles {
 			found, err = doublestar.Match(filepath.ToSlash(pattern), filename)
 			if err != nil {
-				panic(fmt.Sprintf(`error parsing file pattern "%s" - %s`, pattern, err))
+				fmt.Printf("Error parsing file pattern \"%s\" - %s\n", pattern, err)
+				os.Exit(1)
 			}
 			if found {
 				break L
@@ -119,19 +133,13 @@ L:
 
 	if found {
 		result = "exists"
-
-		if fail == "exists" {
-			panic("a file matching the pattern already exists in the package")
-		} else {
-			fmt.Println("A file matching the pattern does exist in the package")
-		}
+		fmt.Println("A file matching the pattern already exists in the package")
 	} else {
 		result = "does-not-exist"
+		fmt.Println("A file matching the pattern does not exist in the package")
+	}
 
-		if fail == "does-not-exist" {
-			panic("a file matching the pattern does not exist in the package")
-		} else {
-			fmt.Println("A file matching the pattern does not exist in the package")
-		}
+	if fail == result {
+		os.Exit(1)
 	}
 }
